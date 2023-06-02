ead5a41cb9bb7f1083fba3c6bfef7d2ecd2d0ebb
Joshua Portero <77929472+joshuaportero@users.noreply.github.com>
2023-06-02 17:51:14 -0400

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "me.joshuaportero.ajs"
+version = "0.1.0-ALPHA"
+
+project.java.sourceCompatibility = JavaVersion.VERSION_17
+project.java.targetCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.seleniumhq.selenium:selenium-java:4.9.1")
+    implementation("org.seleniumhq.selenium:selenium-chrome-driver:4.9.1")
+
+    implementation("org.projectlombok:lombok:1.18.28")
+    annotationProcessor("org.projectlombok:lombok:1.18.28")
+
+    implementation("org.slf4j:slf4j-api:2.0.7")
+    implementation("ch.qos.logback:logback-classic:1.4.7")
+
+    implementation("io.github.cdimascio:dotenv-java:3.0.0")
+    implementation("com.github.lalyos:jfiglet:0.0.8")
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
+    shadowJar {
+        manifest {
+            attributes["Main-Class"] = "me.joshuaportero.ajs.Main"
+        }
+    }
+}
